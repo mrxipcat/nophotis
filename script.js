eb0e6c94d99a5e26cc02246f8b9bab0cf7f5a1e7
mrxipcat <jordi@MacBookAirJS.local>
2026-08-01 11:41:30 +0200

    act2026

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
 const imageLists = {
-    "Colors": [
-	"Black.jpg",
-	"Blond.jpg",
-	"Blue.jpg",
-	"Dark.jpg",
-	"DarkBlue.jpg",
-	"Green.jpg",
-	"BWhite.jpg",
-	"Orange.jpg",
-	"Red.jpg",
-	"Yellow.jpg"
+    "Homepage": [
+"Black.jpg",
+"Blond.jpg",
+"Blue.jpg",
+"Dark.jpg",
+"DarkBlue.jpg",
+"Green.jpg",
+"BWhite.jpg",
+"Maroon.jpg",
+"Orange.jpg",
+"R-DSC03046CES.jpg",
+"R-DSC06705IES.jpg",
+"R-DSC08696ES.jpg",
+"Red.jpg",
+"Yellow.jpg"
     ],
 
     "Landscapes": [
 "Asturies.jpg",
+"Cadiz.jpg",
 "Cala Roca Grossa 1.jpg",
 "Cala Roca Grossa 2.jpg",
 "Cala Roca Grossa 3.jpg",
 "Cala Roca Grossa de nit.jpg",
 "Cami de Ronda a Lloret.jpg",
-"Eivissa Platja De Comte.jpg",
-"Eivissa.jpg",
-"Fecamp.jpg",
-"Linx a Norway.jpg",
-"Lloret.jpg",
-"Lofoten 1.jpg",
-"Lofoten 2.jpg",
-"Lofoten 3.jpg",
-"Lofoten 4.jpg",
-"Lofoten 5.jpg",
-"Menorca 2.jpg",
-"Menorca.jpg",
-"Nao Victoria a Amsterdam.jpg",
-"Nao Victoria Sunset.jpg",
-"Plugia.jpg",
-"BaixadaFalles_1.jpg",
-"BaixadaFalles_2.jpg",
-"Cadiz.jpg",
 "Cantabric.jpg",
 "CliffOfMoher.jpg",
 "EUA_Calico.jpg",
@@ -44,21 +31,21 @@ const imageLists = {
 "EUA_GoldenGate3.jpg",
 "EUA_GranCanyon1.jpg",
 "EUA_GranCanyon2.jpg",
-"Lanzarote_1.jpg",
-"Lanzarote_2.jpg",
-"LofotenBlue.jpg",
+"Eivissa Platja De Comte.jpg",
+"Eivissa.jpg",
+"Fecamp.jpg",
+"Lloret.jpg",
+"Lofoten 5.jpg",
+"Menorca 2.jpg",
 "Menorca 3.jpg",
+"Menorca.jpg",
 "Norway_1.jpg",
 "Norway_2.jpg",
-"Petra_1.jpg",
-"Petra_2.jpg",
-"Suissa_1.jpg",
-"Suissa_2.jpg",
+"R-DSC04141S.jpg",
+"R-DSC04175S.jpg",
+"R-DSC10910S.jpg",
+"R-DSC_7828S.jpg",
 "Suissa_3.jpg",
-"Suissa_4.jpg",
-"Wadirum_1.jpg",
-"Wadirum_2.jpg",
-"Wadirum_3.jpg",
 "Tamariu.jpg"
     ],
 
@@ -69,6 +56,8 @@ const imageLists = {
 "Andrea 6.jpg",
 "Andrea 7.jpg",
 "Carlota 2.jpg",
+"Carol 1.jpg",
+"Carol 2.jpg",
 "Charlie 1.jpg",
 "Charlie 2.jpg",
 "Charlie 3.jpg",
@@ -88,16 +77,14 @@ const imageLists = {
 "Fery 6.jpg",
 "Fery 7.jpg",
 "Jelena 1.jpg",
-"Jelena 10.jpg",
-"Jelena 11.jpg",
-"Jelena 13.jpg",
 "Jelena 2.jpg",
 "Jelena 3.jpg",
 "Jelena 5.jpg",
 "Jelena 6.jpg",
 "Jelena 7.jpg",
 "Jelena 8.jpg",
-"Jelena 9.jpg",
+"Judith 1.jpg",
+"Judith 2.jpg",
 "Julia 2.jpg",
 "Julia 4.jpg",
 "Mary 1.jpg",
@@ -105,20 +92,57 @@ const imageLists = {
 "Mary 3.jpg",
 "Mary 4.jpg",
 "Mary 5.jpg",
+"Monster.jpg",
 "Paula 1.jpg",
 "Paula 3.jpg",
 "Paula 4.jpg",
+"R-DSC00673BES.jpg",
+"R-DSC00807ES.jpg",
+"R-DSC00835BES.jpg",
+"R-DSC00967ES.jpg",
+"R-DSC01217ES.jpg",
+"R-DSC01400ES.jpg",
+"R-DSC01531ES.jpg",
+"R-DSC01531IES.jpg",
+"R-DSC01640IES.jpg",
+"R-DSC01854ES.jpg",
+"R-DSC01897ES.jpg",
+"R-DSC02623ES.jpg",
+"R-DSC03046CES.jpg",
+"R-DSC03143ES.jpg",
+"R-DSC03206S.jpg",
+"R-DSC03431ES.jpg",
+"R-DSC03468ES.jpg",
+"R-DSC03705EAS.jpg",
+"R-DSC03787EAS.jpg",
+"R-DSC03970IEAS.jpg",
+"R-DSC04376ES.jpg",
+"R-DSC04419CES.jpg",
+"R-DSC05004ES.jpg",
+"R-DSC05455ES.jpg",
+"R-DSC05460ES.jpg",
+"R-DSC05730BES.jpg",
+"R-DSC05854BES.jpg",
+"R-DSC05950BES.jpg",
+"R-DSC06133BES.jpg",
+"R-DSC06154BES.jpg",
+"R-DSC06661ES.jpg",
+"R-DSC06689ES.jpg",
+"R-DSC06704IES.jpg",
+"R-DSC06705IES.jpg",
+"R-DSC06708IES.jpg",
+"R-DSC06818ES.jpg",
+"R-DSC06836IES.jpg",
+"R-DSC07529ES.jpg",
+"R-DSC08399ES.jpg",
+"R-DSC08581ES.jpg",
+"R-DSC09899ES.jpg",
+"R-DSC09974ES.jpg",
 "Sofia 1.jpg",
 "Vika 1.jpg",
 "Vika 2.jpg",
 "Vika 3.jpg",
-"Vika 4.jpg",
-"Carol 1.jpg",
-"Carol 2.jpg",
-"Judith 1.jpg",
-"Judith 2.jpg",
-"Monster.jpg",
-"Rontu.jpg"
+"Vika 4.jpg"
 ],
 
     "BlackWhite": [
@@ -140,13 +164,35 @@ const imageLists = {
 "Evgenia 4.jpg",
 "Evgenia 5.jpg",
 "Fery 2.jpg",
-"Jelena 4.jpg",
 "Jelena 12.jpg",
+"Jelena 4.jpg",
 "Julia 1.jpg",
 "Julia 3.jpg",
 "Mary 6.jpg",
 "Mary 7.jpg",
 "Paula 2.jpg",
+"R-DSC02767ES.jpg",
+"R-DSC03202S.jpg",
+"R-DSC03207S.jpg",
+"R-DSC04268ES.jpg",
+"R-DSC04429ES.jpg",
+"R-DSC05389ES.jpg",
+"R-DSC06334ES.jpg",
+"R-DSC07224S.jpg",
+"R-DSC07428ES.jpg",
+"R-DSC07451ES.jpg",
+"R-DSC07582ES.jpg",
+"R-DSC07747S.jpg",
+"R-DSC08158ES.jpg",
+"R-DSC08565ES.jpg",
+"R-DSC08653ES.jpg",
+"R-DSC08696ES.jpg",
+"R-DSC08851ES.jpg",
+"R-DSC08928ES.jpg",
+"R-DSC08976ES.jpg",
+"R-DSC09691ES.jpg",
+"R-DSC10052ES.jpg",
+"R-DSC10077S.jpg",
 "Sofia 2.jpg",
 "Sofia 3.jpg",
 "Vika 5.jpg",
@@ -154,6 +200,14 @@ const imageLists = {
 ],
 
     "Urban": [
+"Autopista.jpg",
+"Belgica_1.jpg",
+"Belgium_2.jpg",
+"Belgium_3.jpg",
+"EUA_GoldenGate1.jpg",
+"EUA_Manhatan.jpg",
+"Napols.jpg",
+"Rome.jpg",
 "Tallin 1.jpg",
 "Tallin 10.jpg",
 "Tallin 11.jpg",
@@ -164,25 +218,28 @@ const imageLists = {
 "Tallin 6.jpg",
 "Tallin 7.jpg",
 "Tallin 8.jpg",
-"Tallin 9.jpg",
-"Belgica_1.jpg",
-"Belgium_2.jpg",
-"Belgium_3.jpg",
-"EUA_Manhatan.jpg",
-"Napols.jpg",
-"Rome.jpg",
-"EUA_GoldenGate1.jpg",
-"Autopista.jpg"
+"Tallin 9.jpg"
 ],
 
     "Sky": [
 "Aurora at Iceland.jpg",
-"Milky Way from Ulldeter.jpg",
+"Aurora at Tromso.jpg",
 "Aurora_3.jpg",
 "AvioLluna.jpg",
 "ErmitaSantQuircdeDurro.jpg",
-"Aurora at Tromso.jpg",
-"Palla nit.jpg"
+"Milky Way from Ulldeter.jpg",
+"Palla nit.jpg",
+"R-Aurora01-9243-9249S.jpg",
+"R-Aurora08-8960-8966S.jpg",
+"R-DSC00167ES.jpg",
+"R-DSC00193BES.jpg",
+"R-DSC04172ES.jpg",
+"R-DSC04191ES.jpg",
+"R-DSC05632ES.jpg",
+"R-DSC05880S.jpg",
+"R-DSC08840ES.jpg",
+"R-VAurora08-0367S.jpg",
+"R-VAurora09-0456S.jpg"
 ],
 
     "Aerial": [
@@ -190,6 +247,87 @@ const imageLists = {
 ],
 
     "Macro": [
+"R-DSC05976S.jpg",
+"R-DSC05991ES.jpg",
+"R-Maduixa.JPG",
+"R-Maduixa_visitant.JPG"
+],
+
+    "Travel": [
+"Lanzarote_1.jpg",
+"Lanzarote_2.jpg",
+"Lofoten 1.jpg",
+"Lofoten 2.jpg",
+"Lofoten 3.jpg",
+"Lofoten 4.jpg",
+"LofotenBlue.jpg",
+"Petra_1.jpg",
+"Petra_2.jpg",
+"Plugia.jpg",
+"R-2023-02-10_18-39-26_800S.jpg",
+"R-2023-02-10_19-01-11_900S.jpg",
+"R-2023-02-12_15-56-59_400S.jpg",
+"R-2023-02-12_16-48-38_800S.jpg",
+"R-DSC_8011S.jpg",
+"R-DSC_8032S.jpg",
+"R-DSC_8440S.jpg",
+"R-DSC_8498S.jpg",
+"R-DSC_9616S.jpg",
+"R-DSC_9645S.jpg",
+"R-Islandia03-8327-8330S.jpg",
+"R-Islandia08-8467-8470S.jpg",
+"Suissa_1.jpg",
+"Suissa_2.jpg",
+"Wadirum_1.jpg",
+"Wadirum_2.jpg",
+"Wadirum_3.jpg"
+],
+
+    "Lifestyle": [
+"BaixadaFalles_1.jpg",
+"BaixadaFalles_2.jpg",
+"Linx a Norway.jpg",
+"Nao Victoria a Amsterdam.jpg",
+"Nao Victoria Sunset.jpg",
+"R-DSC03973S.jpg",
+"R-DSC04045S.jpg",
+"R-DSC04070S.jpg",
+"R-DSC05905S.jpg",
+"R-DSC06245S.jpg",
+"R-DSC06307S.jpg",
+"R-DSC_9455S.jpg",
+"Red.jpg",
+"Rontu.jpg",
+"Suissa_4.jpg",
+"Yellow.jpg"
+],
+
+    "StudioOutdoor": [
+"Jelena 9.jpg",
+"Jelena 10.jpg",
+"Jelena 11.jpg",
+"Jelena 13.jpg",
+"R-DSC04434ES.jpg",
+"R-DSC04517ES.jpg",
+"R-DSC04828IES.jpg",
+"R-DSC04897ES.jpg",
+"R-DSC04924ES.jpg",
+"R-DSC04935ES.jpg",
+"R-DSC05107ES.jpg"
+],
+
+    "History": [
+"R-DSC09077S.jpg",
+"R-DSC09082S.jpg",
+"R-DSC09160ES.jpg",
+"R-DSC09175S.jpg",
+"R-DSC09177S.jpg",
+"R-DSC09201ES.jpg",
+"R-DSC09233ES.jpg",
+"R-DSC09244S.jpg",
+"R-DSC09250ES.jpg",
+"R-DSC09251S.jpg",
+"R-DSC09257S.jpg"
 ]
 };
 
@@ -210,7 +348,7 @@ document.addEventListener('DOMContentLoaded', function () {
 */
     const fullImageContainer = document.getElementById('full-image-container');
 
-    let currentSection = 'Colors';
+    let currentSection = 'Homepage';
     let images = [];
     let currentImageIndex = 0;
 
@@ -245,7 +383,7 @@ function loadImagesFromList(section) {
     } else {
         // Si la categoria no existeix o no té imatges, mostra un missatge
         grid.textContent = '< No photos right now >';
-        console.warn('Categoria no trobada o sense imatges:', category);
+        console.warn('Categoria no trobada o sense imatges:', section);
     }
 }
 
@@ -326,20 +464,15 @@ function loadImagesFromList(section) {
             event.preventDefault();
             const section = this.dataset.section;
             loadImages(section);
-            aboutSection.style.display = 'none';
-            gallery.style.display = 'block';
         });
     });
 
-/*
-    // Event listener per mostrar la secció "Sobre l'Autor"
-    aboutLink.addEventListener('click', function (event) {
+    // El logo actua com a "homepage": mostra la secció Homepage
+    const logoHome = document.getElementById('logo-home');
+    logoHome.addEventListener('click', function (event) {
         event.preventDefault();
-        gallery.style.display = 'none';
-        fullImageView.style.display = 'none';
-        aboutSection.style.display = 'block';
+        loadImages('Homepage');
     });
-*/
 
     // Carregar la secció per defecte al carregar la pàgina
     loadImages(currentSection);
